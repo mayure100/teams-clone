3301d8f11a91e0f093f6e5820b0c71335074773f
foolproof-ux <mayuresh.deshmukh@zensar.com>
2021-02-14 00:42:38 +0530

    teams-clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {ChatEngine} from 'react-chat-engine'
+import LoginForm from './components/LoginForm'
+import './App.css'
+import ChatFeed from './components/ChatFeed'
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm/>
+    return ( 
+        <ChatEngine
+        height='100vh'
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        projectID='9c835370-61f1-4b05-82da-b9e64253c83a'
+        renderChatFeed={(chatAppProps)=><ChatFeed  {...chatAppProps}/>}
+    />
+     );
 }
-
+ 
 export default App;
+
